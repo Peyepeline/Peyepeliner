ba2026ba7e7f91b5f4019dc9e987e5056963564b
Peyepeline <32267500+Peyepeline@users.noreply.github.com>
2017-10-29 21:07:03 +0100

    Add files via upload
    code-snippets für jede bmp-lade-stelle.
    needs trial run; emulator doesn't experience varying dimensions...

---
+++ code_correctionsForImports.java
@@ -0,0 +1,40 @@
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth){
+        // Raw width of image
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+        if (width > reqWidth) {
+            final int halfWidth = width / 2;
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps width larger than the requested width.
+            while((halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
+    }
+
+    public static Bitmap decodeScaledBitmap(String imageFileAbsPath){
+        //while inJustDecodeBounds = true, bitmap remains empty but dimensions and mimeType are readable
+        //therefore: get raw dimensions with iJDB true, recalc, set with iJDB false.
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imageFileAbsPath, options);
+        //calculate scaling
+        int maxWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+        options.inSampleSize = calculateInSampleSize(options, maxWidth);
+        //decode with correct scaling
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(imageFileAbsPath, options);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST) {
+            File imgFile = new  File(pictureImagePath);
+            if(imgFile.exists()){
+                //HERE CALC SIZE AND IMPORT!
+                //Bitmap photo = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                Bitmap photo = decodeScaledBitmap(imgFile.getAbsolutePath());
+                importedPhoto.setImageBitmap(photo);
+            }
+        }
+    }
